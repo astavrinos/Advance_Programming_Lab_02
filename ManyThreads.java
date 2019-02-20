c5e234eae0288ff596f3ead1eed4b1fdc9843266
stavrinoua <stavrinouadamos@hotmail.com>
2019-02-20 02:50:38 +0000

    Progress with Threads

---
+++ ManyThreads.java
@@ -0,0 +1,21 @@
+
+public class ManyThreads implements Runnable {
+
+	public static void main(String[] args) {
+		int nThreads = 2;
+		Thread[] threads = new Thread[nThreads];
+		for(int i = 0; i < nThreads; i++) {
+			PointlessPrint p = new PointlessPrint("I am thread " + i,10);
+			threads[i] = new Thread(p);
+			threads[i].start();
+		}
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
